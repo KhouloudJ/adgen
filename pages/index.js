1e18b7559aa4ac61dd97599bbe401faf3c221f9d
KhouloudJ <khouloudjabloun@hotmail.com>
2022-11-28 13:58:52 +0100

    latest build

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,37 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import adgenLogo from "../assets/adgen_logo.jpg";
+import { useState } from "react";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,22 +40,41 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Bio generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>
+              Add your description and generate a bio for your Twitter page with no hassle on your end.
+            </h2>
+          </div>
+        
+          <div className="prompt-container">
+            <textarea
+              className="prompt-box"
+              placeholder="start typing here"
+              value={userInput}
+              onChange={onUserChangedText}
+            />
+            ;
+          </div>
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={callGenerateEndpoint}>
+              <div className="generate">
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
       <div className="badge-container grow">
         <a
-          href="https://buildspace.so/builds/ai-writer"
+          href="https://www.twitter.com/khdjbn"
           target="_blank"
           rel="noreferrer"
         >
           <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <Image src={adgenLogo} alt="Adgen logo" />
+            <p>build with Adgen</p>
           </div>
         </a>
       </div>
