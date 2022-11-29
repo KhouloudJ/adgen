49fcea2e5e479632a154d6ffa4b03e040fb4d534
KhouloudJ <khouloudjabloun@hotmail.com>
2022-11-29 13:33:21 +0100

    change output text to here is your bio

--- pages/index.js
+++ pages/index.js
@@ -82,7 +82,7 @@ const Home = () => {
             <div className="output">
               <div className="output-header-container">
                 <div className="output-header">
-                  <h3>Output</h3>
+                  <h3>Here is your bio !</h3>
                 </div>
               </div>
               <div className="output-content">
